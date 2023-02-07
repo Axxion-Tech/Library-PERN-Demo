8d6e1ec4d5e16a80a96a038e773e300fab8b68de
Michael Kerr <bulluffalo@gmail.com>
2023-02-07 06:02:46 -0700

    server debugging

--- server/server.js
+++ server/server.js
@@ -10,12 +10,20 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // CONTROLLERS
 const booksController = require('./controllers/books_controller');
 app.use('/api/books', booksController);
 
+console.log('Frontend route:', path.join(__dirname, '../build/index.html'))
+
 // LISTEN
 app.listen(4005, () => {
     console.log('Server is running on port 4005');
+})
+
+app.get('*', (req, res) => {
+    console.log('HIT')
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 })
